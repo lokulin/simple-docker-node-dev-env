bef3075bdf6d5d1aa91519119505ae4717aaaaa2
lokulin <lauchlin@lauchlin.com>
2015-09-03 23:34:56 +1000

    Clean up build context

--- dev/setup_docker.sh
+++ dev/setup_docker.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 . ./app_config.sh
 
-if [[ -f add_user.sh ]]; then
+if [[ -f dev/docker/add_user.sh ]]; then
   GREEN='\033[1;32m'
   NC='\033[0m' # No Color
-  printf "${GREEN}INFO: Skipping updating user. Remove add_user.sh to rebuild.${NC}\n\n"
+  printf "${GREEN}INFO: Skipping updating user. Remove dev/docker/add_user.sh to rebuild.${NC}\n\n"
 else
-  echo "#!/bin/sh" > add_user.sh
-  echo "groupadd -g ${GROUPS} app && useradd -u ${UID} -g ${GROUPS} app" >> add_user.sh
-  chmod +x add_user.sh
-  cat add_user.sh
+  echo "#!/bin/sh" > dev/docker/add_user.sh
+  echo "groupadd -g ${GROUPS} app && useradd -u ${UID} -g ${GROUPS} app" >> dev/docker/add_user.sh
+  chmod +x dev/docker/add_user.sh
+  cat dev/docker/add_user.sh
 fi
 
-docker build -t ${APP_NAME}:dev -f dev.dockerfile .
+docker build -t ${APP_NAME}:dev dev/docker/
 
 # Uncomment once you have a package and/or bower config
 
@@ -22,5 +22,5 @@ docker build -t ${APP_NAME}:dev -f dev.dockerfile .
 # ./dev/run.sh npm install grunt-sass --save-dev
 # ./dev/run.sh bower install angular --save
 
-./dev/run.sh npm install
-./dev/run.sh bower install
+#./dev/run.sh npm install
+#./dev/run.sh bower install

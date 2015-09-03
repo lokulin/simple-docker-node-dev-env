1529a1ec4cbc29d94678e1d213038742998d7bc4
lokulin <lauchlin@lauchlin.com>
2015-09-03 23:54:32 +1000

    Made docker clean up after itself

--- dev/run.sh
+++ dev/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 . ./app_config.sh
 
-docker run -p 9000:9000 -v $(pwd):/app -ti ${APP_NAME}:dev $@
+docker run --rm --read-only --name=${APP_NAME}-dev -p 9000:9000 -v $(pwd):/app -ti ${APP_NAME}:dev $@
--- dev/setup_docker.sh
+++ dev/setup_docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 . ./app_config.sh
 
+[[ "${1}" == "-f" ]] && docker rmi ${APP_NAME}:dev && rm dev/docker/add_user.sh
+
 if [[ -f dev/docker/add_user.sh ]]; then
   GREEN='\033[1;32m'
   NC='\033[0m' # No Color
-  printf "${GREEN}INFO: Skipping updating user. Remove dev/docker/add_user.sh to rebuild.${NC}\n\n"
+  printf "${GREEN}INFO: Soft rebuild. Use -f to force hard rebuild${NC}\n\n"
 else
   echo "#!/bin/sh" > dev/docker/add_user.sh
   echo "groupadd -g ${GROUPS} app && useradd -u ${UID} -g ${GROUPS} app" >> dev/docker/add_user.sh
